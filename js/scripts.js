78c9ef7b43914bc83de5155e82a6152ab70a60c3
Trevor Fast and Tyler Stephenson <tyler@>
2017-03-28 16:14:46 -0700

    add shuffle method to puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,26 @@
 //   return newString;
 // }
 
+// Stolen from stackoverflow
+String.prototype.shuffle = function () { // <-- this is mixing all letters between all words.
+  var a = this.split(""),
+      n = a.length;
+
+  for(var i = n - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1)); // <-- this makes it randomize to a new combination every time you reload the page.
+      var tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+  }
+  return a.join("");
+};
+// end
+
 $(function(){
   $('.answer, .error').hide();
   var answer1 = "Believe you can and you're halfway there. Theodore Roosevelt";
   var vowels = ["a", "e", "i", "o", "u"];
-  var replacedVowels = answer1.substr(0);
+  var replacedVowels = answer1.substr(0).shuffle();
 
   for (i = 0; i < vowels.length; i++) {
     var replacedVowels = replacedVowels.split(vowels[i]).join("-");
