f3c94bc64ea528f4f33fc6c3479a2774154c563e
Trevor Fast and Tyler Stephenson <tyler@>
2017-03-28 11:39:56 -0700

    create base site with logic for word puzzles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(function(){
+  $('.answer, .error').hide();
+  var answer1 = "Believe you can and you're halfway there. Theodore Roosevelt";
+  var vowels = ["a", "e", "i", "o", "u"];
+  var replacedVowels = answer1.substr(0);
 
+  for (i = 0; i < vowels.length; i++) {
+    var replacedVowels = replacedVowels.split(vowels[i]).join("-");
+  };
 
-  
+  $('.puzzle h2').text(replacedVowels);
+
+  $('.userInput button').click(function() {
+    var userInput = $('.userInput input').val();
+    if (userInput === answer1){
+      $('.intial').fadeOut();
+      $('.answer').fadeIn();
+      $('.answer h3').text(answer1);
+    } else {
+      // $('.userInput input').effect('shake');
+      $('.error').show();
+      $('.userInput input').addClass('input-error');
+    }
+  });
 });
